f8b85d18cb9866f1d3a8617ac0ac4a29c24bfe04
makter20 <74988056+makter20@users.noreply.github.com>
2021-07-18 13:00:37 -0700

    added jdbc driver

---
+++ src/main/java/tester.java
@@ -0,0 +1,17 @@
+import java.sql.*;
+
+public class tester {
+
+	public static void main(String[] args) throws SQLException {
+		
+		Connection con = WebConnection.getConnection();
+		DatabaseMetaData dbMeta = con.getMetaData();
+		System.out.println(dbMeta.getDatabaseProductName());
+		System.out.println(dbMeta.getDriverName());
+		System.out.println(dbMeta.getDatabaseProductVersion());
+		con.close();
+		
+
+	}
+
+}
